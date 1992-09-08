098811d6cb39f3a7bc4e95642c65b222c425494b
Ian Darwin <ian@darwinsys.com>
1992-09-08 15:32:41 +0000

    Add RCS ID keywords.

--- src/tar.h
+++ src/tar.h
@@ -4,6 +4,8 @@
  * @(#)tar.h 1.20 86/10/29	Public Domain.
  *
  * Created 25 August 1985 by John Gilmore, ihnp4!hoptoad!gnu.
+ *
+ * $Id: tar.h,v 1.3 1992/09/08 15:32:41 ian Exp $ # checkin only
  */
 
 /*
