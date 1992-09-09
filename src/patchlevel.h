e17e5c5ce8c6bb3a661a7ac82190c36f665d5860
Ian Darwin <ian@darwinsys.com>
1992-09-09 15:06:00 +0000

    Minor level 2.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -1,7 +1,7 @@
 /*
  * Patchlevel file for Ian Darwin's MAGIC command.
- * $Id: patchlevel.h,v 1.1 1992/09/08 15:32:33 ian Exp $
+ * $Id: patchlevel.h,v 1.2 1992/09/09 15:06:00 ian Exp $
  */
 
 #define	FILE_VERSION_MAJOR	3
-#define	patchlevel		0
+#define	patchlevel		2
