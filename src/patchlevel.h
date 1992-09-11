f80b704da07b76538cf6028a3e9bfaa3e93e34aa
Ian Darwin <ian@darwinsys.com>
1992-09-11 10:12:10 +0000

    Christos' fix for compressed tar archives failing checksum due to strtok tromping the buffer and causing the checksum to fail.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -1,7 +1,7 @@
 /*
  * Patchlevel file for Ian Darwin's MAGIC command.
- * $Id: patchlevel.h,v 1.2 1992/09/09 15:06:00 ian Exp $
+ * $Id: patchlevel.h,v 1.3 1992/09/11 10:12:10 ian Exp $
  */
 
 #define	FILE_VERSION_MAJOR	3
-#define	patchlevel		2
+#define	patchlevel		3
