c0e09acda20d53595e70d057c3c52416b9ea2ec8
Ian Darwin <ian@darwinsys.com>
1992-09-11 17:47:06 +0000

    Zoulas' nitpicking.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -1,7 +1,7 @@
 /*
  * Patchlevel file for Ian Darwin's MAGIC command.
- * $Id: patchlevel.h,v 1.3 1992/09/11 10:12:10 ian Exp $
+ * $Id: patchlevel.h,v 1.4 1992/09/11 17:47:06 ian Exp $
  */
 
 #define	FILE_VERSION_MAJOR	3
-#define	patchlevel		3
+#define	patchlevel		4
