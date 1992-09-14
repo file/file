8439bea6068713cf570c0124f2bdbb68c0ad0362
Ian Darwin <ian@darwinsys.com>
1992-09-14 14:54:51 +0000

    Fix a tiny null-pointer bug in previous fix for tar archive + uncompress.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -1,7 +1,12 @@
 /*
  * Patchlevel file for Ian Darwin's MAGIC command.
- * $Id: patchlevel.h,v 1.4 1992/09/11 17:47:06 ian Exp $
+ * $Id: patchlevel.h,v 1.5 1992/09/14 14:54:51 ian Exp $
+ *
+ * $Log: patchlevel.h,v $
+ * Revision 1.5  1992/09/14 14:54:51  ian
+ * Fix a tiny null-pointer bug in previous fix for tar archive + uncompress.
+ *
  */
 
 #define	FILE_VERSION_MAJOR	3
-#define	patchlevel		4
+#define	patchlevel		5
