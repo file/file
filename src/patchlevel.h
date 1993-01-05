3abc059eb26e6e88035762cec100f7e112b5b9ab
Ian Darwin <ian@darwinsys.com>
1993-01-05 14:57:27 +0000

    Couple of nits picked by Christos (again, thanks).

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -1,15 +1,18 @@
 /*
  * Patchlevel file for Ian Darwin's MAGIC command.
- * $Id: patchlevel.h,v 1.6 1993/01/05 13:51:09 ian Exp $
+ * $Id: patchlevel.h,v 1.7 1993/01/05 14:57:27 ian Exp $
  *
  * $Log: patchlevel.h,v $
- * Revision 1.6  1993/01/05 13:51:09  ian
- * Lotsa work on the Magic directory.
+ * Revision 1.7  1993/01/05 14:57:27  ian
+ * Couple of nits picked by Christos (again, thanks).
  *
+ * Revision 1.6  93/01/05  13:51:09  ian
+ * Lotsa work on the Magic directory.
+ * 
  * Revision 1.5  92/09/14  14:54:51  ian
  * Fix a tiny null-pointer bug in previous fix for tar archive + uncompress.
  * 
  */
 
 #define	FILE_VERSION_MAJOR	3
-#define	patchlevel		6
+#define	patchlevel		7
