8c8fea8bc69325fe7f6cc48fc1e1a6bb1a4174e6
Ian Darwin <ian@darwinsys.com>
1993-01-05 13:51:09 +0000

    Lotsa work on the Magic directory.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -1,12 +1,15 @@
 /*
  * Patchlevel file for Ian Darwin's MAGIC command.
- * $Id: patchlevel.h,v 1.5 1992/09/14 14:54:51 ian Exp $
+ * $Id: patchlevel.h,v 1.6 1993/01/05 13:51:09 ian Exp $
  *
  * $Log: patchlevel.h,v $
- * Revision 1.5  1992/09/14 14:54:51  ian
- * Fix a tiny null-pointer bug in previous fix for tar archive + uncompress.
+ * Revision 1.6  1993/01/05 13:51:09  ian
+ * Lotsa work on the Magic directory.
  *
+ * Revision 1.5  92/09/14  14:54:51  ian
+ * Fix a tiny null-pointer bug in previous fix for tar archive + uncompress.
+ * 
  */
 
 #define	FILE_VERSION_MAJOR	3
-#define	patchlevel		5
+#define	patchlevel		6
