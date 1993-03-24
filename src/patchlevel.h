132eec8dc6507089e9da1ca539e0ffd5bfdfef55
Ian Darwin <ian@darwinsys.com>
1993-03-24 14:23:40 +0000

    Batch of minor changes from several contributors.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -1,16 +1,19 @@
 #define	FILE_VERSION_MAJOR	3
-#define	patchlevel		8
+#define	patchlevel		9
 
 /*
  * Patchlevel file for Ian Darwin's MAGIC command.
- * $Id: patchlevel.h,v 1.8 1993/02/19 15:01:26 ian Exp $
+ * $Id: patchlevel.h,v 1.9 1993/03/24 14:23:40 ian Exp $
  *
  * $Log: patchlevel.h,v $
- * Revision 1.8  1993/02/19 15:01:26  ian
+ * Revision 1.9  1993/03/24 14:23:40  ian
+ * Batch of minor changes from several contributors.
+ *
+ * Revision 1.8  93/02/19  15:01:26  ian
  * Numerous changes from Guy Harris too numerous to mention but including
  * byte-order independance, fixing "old-style masking", etc. etc. A bugfix
  * for broken symlinks from martin@@d255s004.zfe.siemens.de.
- *
+ * 
  * Revision 1.7  93/01/05  14:57:27  ian
  * Couple of nits picked by Christos (again, thanks).
  * 
