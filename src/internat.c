827f56497341ba8336daf9c716fad88088ce84e9
Christos Zoulas <christos@zoulas.com>
1997-04-13 18:29:26 +0000

    Added joerg's license.

--- src/internat.c
+++ src/internat.c
@@ -1,3 +1,12 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <joerg@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Joerg Wunsch
+ * ----------------------------------------------------------------------------
+ */
+
 #include "file.h"
 
 #include <string.h>
