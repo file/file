3ac1dafef274b4f13f0219388805b0f123c2c963
Christos Zoulas <christos@zoulas.com>
2007-03-02 19:29:55 +0000

    ugly hack to override const.

--- src/softmagic.c
+++ src/softmagic.c
@@ -38,7 +38,7 @@
 
 
 #ifndef	lint
-FILE_RCSID("@(#)$File: softmagic.c,v 1.92 2007/03/02 17:50:06 christos Exp $")
+FILE_RCSID("@(#)$File: softmagic.c,v 1.93 2007/03/02 19:29:55 christos Exp $")
 #endif	/* lint */
 
 private int match(struct magic_set *, struct magic *, uint32_t,
@@ -1525,8 +1525,9 @@ magiccheck(struct magic_set *ms, struct magic *m)
 			regmatch_t pmatch[1];
 #ifndef REG_STARTEND
 #define	REG_STARTEND	0
-			char c = ms->search.s[ms->search.s_len - 1];
-			ms->search.s[ms->search.s_len - 1] = '\0';
+			size_t l = ms->search.s_len - 1;
+			char c = ms->search.s[l]
+			((char *)(intptr_t)ms->search.s)[l] = '\0';
 #else
 			pmatch[0].rm_so = 0;
 			pmatch[0].rm_eo = ms->search.s_len;
@@ -1534,7 +1535,7 @@ magiccheck(struct magic_set *ms, struct magic *m)
 			rc = regexec(&rx, (const char *)ms->search.s,
 			    1, pmatch, REG_STARTEND);
 #if REG_STARTEND == 0
-			ms->search.s[ms->search.s_len - 1] = c;
+			((char *)(intptr_t)ms->search.s)[l] = c;
 #endif
 			switch (rc) {
 			case 0:
