92b5af700621ad9649aa0190b5678c980df1d989
Christos Zoulas <christos@zoulas.com>
2007-12-27 16:38:24 +0000

    welcome to 4.22

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -1,11 +1,14 @@
 #define	FILE_VERSION_MAJOR	4
-#define	patchlevel		21
+#define	patchlevel		22
 
 /*
  * Patchlevel file for Ian Darwin's MAGIC command.
- * $File: patchlevel.h,v 1.64 2007/03/01 22:14:55 christos Exp $
+ * $File: patchlevel.h,v 1.65 2007/05/24 17:22:27 christos Exp $
  *
  * $Log: patchlevel.h,v $
+ * Revision 1.66  2007/12/27 16:38:24  christos
+ * welcome to 4.22
+ *
  * Revision 1.65  2007/05/24 17:22:27  christos
  * Welcome to 4.21
  *
