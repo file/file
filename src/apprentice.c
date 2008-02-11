812ade5fea9e58fa45b0f8f5d8786ece397d959a
Reuben Thomas <rrt@sc3d.org>
2008-02-11 22:12:24 +0000

    Add period (.) to allowable MIME type character list

--- src/apprentice.c
+++ src/apprentice.c
@@ -47,7 +47,7 @@
 #endif
 
 #ifndef	lint
-FILE_RCSID("@(#)$File: apprentice.c,v 1.113 2008/02/04 20:51:17 christos Exp $")
+FILE_RCSID("@(#)$File: apprentice.c,v 1.114 2008/02/11 01:36:08 rrt Exp $")
 #endif	/* lint */
 
 #define	EATAB {while (isascii((unsigned char) *l) && \
@@ -1243,7 +1243,7 @@ parse_mime(struct magic_set *ms, struct magic_entry **mentryp,
 	EATAB;
 	for (i = 0;
 	     *l && ((isascii((unsigned char)*l) && isalnum((unsigned char)*l))
-	     || strchr("-+/", *l)) && i < sizeof(m->mimetype);
+	     || strchr("-+/.", *l)) && i < sizeof(m->mimetype);
 	     m->mimetype[i++] = *l++)
 		continue;
 	if (i == sizeof(m->mimetype)) {
