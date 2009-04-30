80e7810793346d04bdf5e81e28b78f3628c6f444
Christos Zoulas <christos@zoulas.com>
2009-04-30 21:20:15 +0000

    5.01 we are almost here.

--- src/patchlevel.h
+++ src/patchlevel.h
@@ -1,11 +1,14 @@
 #define	FILE_VERSION_MAJOR	5
-#define	patchlevel		0
+#define	patchlevel		1
 
 /*
  * Patchlevel file for Ian Darwin's MAGIC command.
- * $File: patchlevel.h,v 1.71 2009/01/21 19:09:42 christos Exp $
+ * $File: patchlevel.h,v 1.72 2009/04/30 21:20:15 christos Exp $
  *
  * $Log: patchlevel.h,v $
+ * Revision 1.72  2009/04/30 21:20:15  christos
+ * 5.01 we are almost here.
+ *
  * Revision 1.71  2009/01/21 19:09:42  christos
  * file 5.0
  *
