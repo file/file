6cb5ecba732ef067bf5500994340850b53652945
Christos Zoulas <christos@zoulas.com>
2010-12-15 02:49:24 +0000

    update for python-3.1 from Kelly Anderson.

--- python/setup.py
+++ python/setup.py
@@ -1,10 +1,17 @@
 # Python distutils build script for magic extension
+import sys, os
 from distutils.core import setup, Extension
+
+p = sys.prefix + '/lib64'
+if os.path.exists(sys.prefix + '/lib64'):
+    libdir = sys.prefix + '/lib64'
+elif os.path.exists(sys.prefix + '/lib'):
+    libdir = sys.prefix + '/lib'
 
 magic_module = Extension('magic',
     libraries = ['magic'],
-    library_dirs = ['./','../','../src','/usr/lib/'],
-    include_dirs = ['./','../','../src','/usr/include/'],
+    library_dirs = ['./', '../', '../src', libdir],
+    include_dirs = ['./', '../', '../src', sys.prefix + '/include' ],
     sources = ['py_magic.c'])
 
 setup (name = 'Magic file extensions',
