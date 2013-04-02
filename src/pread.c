abfef0af87ff572c281f24b969afa88f7b8b0bd8
Christos Zoulas <christos@zoulas.com>
2013-04-02 16:23:07 +0000

    fix wrong type, from joerg jenderek

--- src/pread.c
+++ src/pread.c
@@ -1,12 +1,12 @@
 #include "file.h"
 #ifndef lint
-FILE_RCSID("@(#)$File: ctime_r.c,v 1.1 2012/05/15 17:14:36 christos Exp $")
+FILE_RCSID("@(#)$File: pread.c,v 1.1 2013/02/18 15:40:59 christos Exp $")
 #endif  /* lint */
 #include <fcntl.h>
 #include <unistd.h>
 
 ssize_t
-pread(int fd, void *buf, ssize_t len, off_t off) {
+pread(int fd, void *buf, size_t len, off_t off) {
 	if (lseek(fd, off, SEEK_SET) == (off_t)-1)
 		return -1;
 
