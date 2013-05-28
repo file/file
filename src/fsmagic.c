fb6084e0f08aef8991afcb3eb74168a456601908
Christos Zoulas <christos@zoulas.com>
2013-05-28 21:24:31 +0000

    don't print a space if there was an error. (from Jan Kaluza)

--- src/fsmagic.c
+++ src/fsmagic.c
@@ -32,7 +32,7 @@
 #include "file.h"
 
 #ifndef	lint
-FILE_RCSID("@(#)$File: fsmagic.c,v 1.66 2013/01/11 19:46:55 christos Exp $")
+FILE_RCSID("@(#)$File: fsmagic.c,v 1.67 2013/03/17 15:43:20 christos Exp $")
 #endif	/* lint */
 
 #include "magic.h"
@@ -367,7 +367,7 @@ file_fsmagic(struct magic_set *ms, const char *fn, struct stat *sb)
 		/*NOTREACHED*/
 	}
 
-	if (!mime && did) {
+	if (!mime && did && ret == 0) {
 	    if (file_printf(ms, " ") == -1)
 		    return -1;
 	}
