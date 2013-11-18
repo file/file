33312fd828e59c78ae4ee30fd70d0ca109748cf0
Christos Zoulas <christos@zoulas.com>
2013-11-18 17:54:58 +0000

    don't write a char to a pointer.

--- src/compress.c
+++ src/compress.c
@@ -35,7 +35,7 @@
 #include "file.h"
 
 #ifndef lint
-FILE_RCSID("@(#)$File: compress.c,v 1.70 2012/11/07 17:54:48 christos Exp $")
+FILE_RCSID("@(#)$File: compress.c,v 1.71 2013/07/21 21:06:41 rrt Exp $")
 #endif
 
 #include "magic.h"
@@ -479,7 +479,7 @@ uncompressbuf(struct magic_set *ms, int fd, size_t method,
 #endif
 			free(*newch);
 			n = 0;
-			newch[0] = '\0';
+			*newch = NULL;
 			goto err;
 		} else {
 			n = r;
