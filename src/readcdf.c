6e4e1f1efdcc0843739c61ab836794938e9af59a
Reuben Thomas <rrt@sc3d.org>
2014-01-06 13:41:18 +0000

    Fix cdf_app_to_mime, broken in previous commit: actually return the return value

--- src/readcdf.c
+++ src/readcdf.c
@@ -26,7 +26,7 @@
 #include "file.h"
 
 #ifndef lint
-FILE_RCSID("@(#)$File: readcdf.c,v 1.36 2013/12/05 17:02:34 christos Exp $")
+FILE_RCSID("@(#)$File: readcdf.c,v 1.37 2014/01/06 13:41:18 rrt Exp $")
 #endif
 
 #include <stdlib.h>
@@ -82,7 +82,7 @@ cdf_app_to_mime(const char *vbuf, const struct nv *nv)
 			break;
 		}
 	(void)setlocale(LC_CTYPE, "");
-	return NULL;
+	return rv;
 }
 
 private int
