64e1d2d6a37ffe309e07347cb3e70512153f643c
Christos Zoulas <christos@zoulas.com>
2023-05-24 16:54:16 +0000

    need stddef.h for ptrdiff_t

--- src/is_simh.c
+++ src/is_simh.c
@@ -33,10 +33,11 @@
 #include "file.h"
 
 #ifndef lint
-FILE_RCSID("@(#)$File: is_simh.c,v 1.4 2023/05/23 20:13:47 christos Exp $")
+FILE_RCSID("@(#)$File: is_simh.c,v 1.5 2023/05/24 16:54:16 christos Exp $")
 #endif
 
 #include <string.h>
+#include <stddef.h>
 #include "magic.h"
 #else
 #include <sys/types.h>
