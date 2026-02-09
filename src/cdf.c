620b4bb7dd70452d5f729ddd54be6d9f0dd69d60
Thomas Ledoux <tledouxfr@gmail.com>
2026-02-09 16:39:41 +0100

    Error in cdf_find_stream still need to return the same index for directories

--- src/cdf.c
+++ src/cdf.c
@@ -880,15 +880,15 @@ cdf_find_stream(const cdf_dir_t *dir, const char *name, int type)
 {
 	size_t i, name_len = strlen(name) + 1;
 
-	for (i = 0; i < dir->dir_len; i++)
-		if (dir->dir_tab[i].d_type == type &&
-		    cdf_namecmp(name, dir->dir_tab[i].d_name, name_len)
+	for (i = 1; i <= dir->dir_len; i++)
+		if (dir->dir_tab[i-1].d_type == type &&
+		    cdf_namecmp(name, dir->dir_tab[i-1].d_name, name_len)
 		    == 0)
 			break;
-	if (i > 0)
+	if (i <= dir->dir_len)
 		return CAST(int, i);
 
-	DPRINTF(("Cannot find type %d `%s'\n", type, name));
+	DPRINTF(("Cannot find type %d '%s'\n", type, name));
 	errno = ESRCH;
 	return 0;
 }
